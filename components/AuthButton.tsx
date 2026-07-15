499fa3a279c2f31b6b88cf7a4984c92b9e3ecdc2
Josh <15219491+jkaps9@users.noreply.github.com>
2026-07-14 23:14:05 -0400

    update login flow

---
+++ components/AuthButton.tsx
@@ -0,0 +1,29 @@
+import { createClient } from "@/lib/supabase/server";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+export default async function AuthButton() {
+  const supabase = await createClient();
+
+  const { data } = await supabase.auth.getClaims();
+  const claims = data?.claims;
+
+  const signOut = async () => {
+    "use server";
+
+    const supabase = await createClient();
+    await supabase.auth.signOut();
+    return redirect("/login");
+  };
+
+  return claims ? (
+    <div className="flex items-center gap-4">
+      Hey, {claims.email}!
+      <form action={signOut}>
+        <button>Logout</button>
+      </form>
+    </div>
+  ) : (
+    <Link href="/login">Login</Link>
+  );
+}
