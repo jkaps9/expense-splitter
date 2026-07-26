28ebdc948aa8a000a61c78eeec15004d2a4873f7
Josh <15219491+jkaps9@users.noreply.github.com>
2026-07-25 23:27:11 -0400

    attempt to redirect authenticated users to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { createClient } from "@/lib/supabase/client";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+
 import siteConfig from "@/data/site-config.json";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+    error: authError,
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    redirect("/dashboard");
+  }
+
+  if (authError) {
+    redirect("/login");
+  }
   return (
     <section className="hero">
       <div className="container">
+        <Link href="/login">Log In / Sign Up</Link>
         <h1>{siteConfig.siteName}</h1>
         <p>{siteConfig.siteDescription}</p>
       </div>
