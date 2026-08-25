fdded4a94fce596dabba847e2d3b17eda4bcb513
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-24 21:44:07 -0400

    feat: implement delete functionality for groups in GroupDetails component

--- src/routes/groups/group-details.tsx
+++ src/routes/groups/group-details.tsx
@@ -1,12 +1,30 @@
 import { useParams } from "react-router";
+import { supabase } from "@/lib/supabase";
 
 export default function GroupDetails() {
   const { id } = useParams();
+
+  const deleteGroup = async () => {
+    const response = await supabase.from("groups").delete().eq("id", id);
+
+    if (response.success) {
+      alert("group deleted");
+    } else {
+      alert(`something went wrong\n${response.status}: ${response.statusText}`);
+    }
+  };
 
   return (
     <>
       <h1>{id}</h1>
       <p>Dynamic route! Yeehaw!</p>
+      <button
+        type="button"
+        className="btn btn--destructive"
+        onClick={deleteGroup}
+      >
+        Delete Group
+      </button>
     </>
   );
 }
