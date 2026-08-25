7990fbe7bc9d4124a9581392ea6b6150a79ee9e8
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-24 21:16:22 -0400

    feat: add TODO comments for form fields in NewGroup component

--- src/routes/groups/new-group.tsx
+++ src/routes/groups/new-group.tsx
@@ -5,6 +5,18 @@ export default function NewGroup() {
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, dicta!
       </p>
+
+      {/* TODO: form fields to add: 
+          - name
+          - description
+          - default_currency
+          - Group Members
+            - guest_name OR user_id
+          - type (bonus - not currently in db)
+          - avatar (bonus - not currently in db)
+          - settle up day (bonus - not currently in db)
+          - simplify group debts? (bonus - not currently db)
+      */}
     </>
   );
 }
