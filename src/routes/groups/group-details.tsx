4c26d82087e5c802a3992ee1aea8e9cda04a8f32
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-24 21:46:01 -0400

    feat: add navigation functionality and delete group confirmation in GroupDetails component

--- src/routes/groups/group-details.tsx
+++ src/routes/groups/group-details.tsx
@@ -1,14 +1,17 @@
-import { useParams } from "react-router";
+import { useParams, useNavigate } from "react-router";
 import { supabase } from "@/lib/supabase";
 
 export default function GroupDetails() {
   const { id } = useParams();
+
+  const navigate = useNavigate();
 
   const deleteGroup = async () => {
     const response = await supabase.from("groups").delete().eq("id", id);
 
     if (response.success) {
       alert("group deleted");
+      navigate(-1);
     } else {
       alert(`something went wrong\n${response.status}: ${response.statusText}`);
     }
@@ -16,6 +19,9 @@ export default function GroupDetails() {
 
   return (
     <>
+      <button type="button" onClick={() => navigate(-1)}>
+        Go Back
+      </button>
       <h1>{id}</h1>
       <p>Dynamic route! Yeehaw!</p>
       <button
