61dba2af5e50805d34cee6941c83fac922de35fc
Josh <15219491+jkaps9@users.noreply.github.com>
2026-09-02 01:33:55 -0400

    feat: implement GroupDetails component with data fetching and loading state

--- src/routes/groups/group-details.tsx
+++ src/routes/groups/group-details.tsx
@@ -1,8 +1,59 @@
 import { useParams, useNavigate } from "react-router";
 import { supabase } from "@/lib/supabase";
+import { Group } from "@/types";
+import { useState, useEffect } from "react";
 
 export default function GroupDetails() {
+  const [loading, setLoading] = useState(true);
+  const [groupDetails, setGroupDetails] = useState<Group>({
+    id: "",
+    name: "",
+    description: "",
+    default_currency: "",
+    created_at: "",
+  });
+
   const { id } = useParams();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const [groupDetailRes] = await Promise.all([
+          supabase.from("groups").select("*").eq("id", id),
+        ]);
+
+        if (groupDetailRes.error) {
+          console.error("Error fetching groups", groupDetailRes.error.message);
+        } else {
+          setGroupDetails(groupDetailRes.data[0]);
+        }
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        if (event === "SIGNED_OUT" || !session) {
+          setGroupDetails({
+            id: "",
+            name: "",
+            description: "",
+            default_currency: "",
+            created_at: "",
+          });
+        }
+      },
+    );
+
+    return () => {
+      authListener.subscription.unsubscribe();
+    };
+  });
 
   const navigate = useNavigate();
 
@@ -19,15 +70,20 @@ export default function GroupDetails() {
 
   return (
     <>
-      <h1>{id}</h1>
-      <p>Dynamic route! Yeehaw!</p>
-      <button
-        type="button"
-        className="btn btn--destructive"
-        onClick={deleteGroup}
-      >
-        Delete Group
-      </button>
+      <div className="row">
+        {loading && <p>Loading...</p>}
+        <div>
+          <h1>{groupDetails.name}</h1>
+          <p>{groupDetails.description}</p>
+        </div>
+        <button
+          type="button"
+          className="btn btn--destructive"
+          onClick={deleteGroup}
+        >
+          Delete Group
+        </button>
+      </div>
     </>
   );
 }
