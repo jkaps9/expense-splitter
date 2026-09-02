651824273ce3cb313a8701b5db1494a27bfc13d7
Josh <15219491+jkaps9@users.noreply.github.com>
2026-09-02 01:17:01 -0400

    feat: add create group form

--- src/routes/groups/new-group.tsx
+++ src/routes/groups/new-group.tsx
@@ -1,11 +1,81 @@
+import FormInput from "@/components/FormInput";
+import { supabase } from "@/lib/supabase";
+import { useForm } from "@/hooks/useFormValidation";
+import { useNavigate } from "react-router";
+
 export default function NewGroup() {
+  const navigate = useNavigate();
+
+  const { formData, errors, handleChange, handleSubmit } = useForm({
+    initialValues: { name: "", description: "", default_currency: "" },
+    validate: (values) => {
+      const newErrors: Partial<Record<keyof typeof values, string>> = {};
+      if (!values.name) newErrors.name = "Can't be empty";
+      if (!values.default_currency)
+        newErrors.default_currency = "Can't be blank";
+      return newErrors;
+    },
+    onSubmit: async (values) => {
+      const { data, error } = await supabase
+        .from("groups")
+        .insert({
+          name: values.name,
+          description: values.description,
+          default_currency: values.default_currency,
+        })
+        .select()
+        .single();
+
+      if (error) {
+        alert(error.message);
+      } else {
+        navigate(`${import.meta.env.BASE_URL}/groups/${data.id}`);
+      }
+    },
+  });
+
   return (
     <>
       <h1>Create new group</h1>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, dicta!
       </p>
-
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          id="groupName"
+          name="name"
+          label="Name"
+          type="text"
+          placeholder="Family Vacation"
+          onChange={handleChange}
+          errorMessage={errors.name}
+          value={formData.name}
+          required
+        ></FormInput>
+        <FormInput
+          id="groupDescription"
+          name="description"
+          label="Description"
+          type="text"
+          placeholder="Describe the purpose of the group"
+          value={formData.description}
+          onChange={handleChange}
+        ></FormInput>
+        <FormInput
+          id="groupDefaultCurrency"
+          name="default_currency"
+          label="Default Currency"
+          type="text"
+          placeholder="USD"
+          onChange={handleChange}
+          errorMessage={errors.default_currency}
+          value={formData.default_currency}
+          required
+        ></FormInput>
+        <button type="submit" className="btn btn--primary">
+          Create Group
+        </button>
+      </form>
       {/* TODO: form fields to add: 
           - name
           - description
