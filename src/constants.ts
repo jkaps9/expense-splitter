5f3c0facca33025adc4e8a132a368b9748539bd1
Josh <15219491+jkaps9@users.noreply.github.com>
2026-09-06 11:12:41 -0400

    feat: add CURRENCIES constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const SPLIT_TYPES = ["Equal", "Exact", "Percentage", "Shares"] as const;
+export const SPLIT_TYPES = ["Equal", "Exact", "Percentage", "Shares"];
 
 export type SplitTypes = (typeof SPLIT_TYPES)[number];
 
@@ -15,3 +15,17 @@ export const EXPENSE_CATEGORIES = [
 ];
 
 export type ExpenseCategories = (typeof EXPENSE_CATEGORIES)[number];
+
+export const CURRENCIES = [
+  { iso_code: "USD", symbol: "$" },
+  { iso_code: "EUR", symbol: "€" },
+  { iso_code: "JPY", symbol: "¥" },
+  { iso_code: "GBP", symbol: "£" },
+  { iso_code: "CNY", symbol: "¥" },
+  { iso_code: "AUD", symbol: "$" },
+  { iso_code: "CAD", symbol: "$" },
+  { iso_code: "HKD", symbol: "$" },
+  { iso_code: "SGD", symbol: "$" },
+];
+
+export type Currencies = (typeof CURRENCIES)[number];
